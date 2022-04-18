ba0e5176af57de86647605254932035a64cc7018
castberry10 <soc09072@gmail.com>
2022-04-18 20:42:56 +0900

    Solution

--- sh-script/chkSudoers4.sh
+++ sh-script/chkSudoers4.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#grepstring= grep "${LOGNAME}" /etc/sudoers
-`echo sudo 1 > sudook.txt`
-
 helpstring(){
     echo "usage: $0 [option]"
     echo "-h  --help            print this usage and exit"
 }
-
 INPUT_FILE=""
 OPT_INT=0
 OPT_BOOL=False
@@ -16,18 +12,38 @@ while (("$#")); do
                 -h|--help)
                     helpstring
                     exit 0;;
-                -*|--*)
-                    "$0: invalid option -- '$1'"
+
+                *)
+                    echo "$0: invalid option -- '$1'"
                     helpstring
                     exit 1;;
 
-                *)
-                    "$0: invalid option -- '$1'"
-                    helpstring
-                    exit 1;;
-                ?)
-                    "$0: invalid option -- '$1'"
+                -*|--*)
+                    echo "$0:: invalid option -- '$1'"
                     helpstring
                     exit 1;;
         esac
 done
+
+#debug
+if [ ${LOGNAME} == wychoi02 ]
+then
+    echo "b"
+fi 
+#end
+
+if [ ${LOGNAME} == asdf2]
+then
+    echo "You're one of sudoers"
+    exit 0
+fi
+
+if [ ${LOGNAM} == asdf] 
+then
+    echo "You're NOT one of sudoer"
+    exit 0
+fi 
+
+# if not in all cases
+echo "You're NOT on of sudoer"
+exit 0 
